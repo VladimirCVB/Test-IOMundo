98e02382bc370dec9afd922810f457e43276098b
VladimirCVB <abuhnoaeivladimir@gmail.com>
2023-07-31 13:49:22 +0300

    feat: Add Offer Repository
    Add OfferRepository logic to be used in the OfferController.

---
+++ Test-IOMundo.tests/Repository/OfferRepositoryTets.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Test_IOMundo.Data;
+using Test_IOMundo.Models;
+using Test_IOMundo.Repository;
+using Test_IOMundo.ViewModels;
+using Xunit;
+
+namespace Test_IOMundo.tests.Repository
+{
+    public class OfferRepositoryTets
+    {
+        private async Task<ApplicationDbContext> GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            var databaseContext = new ApplicationDbContext(options);
+            databaseContext.Database.EnsureCreated();
+            if (await databaseContext.Offers.CountAsync() < 0)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    databaseContext.Offers.Add(
+                      new Offer()
+                      {
+                          CheckInDate = DateTime.Now,
+                          StayDurationNights = 10,
+                          PersonCombination = "10",
+                          ServiceCode = "123AB",
+                          Price = 50,
+                          PricePerAdult = 10,
+                          PricePerChild = 5,
+                          StrikePrice = 10,
+                          StrikePricePerAdult = 10,
+                          StrikePricePerChild = 5,
+                          ShowStrikePrice = true,
+                          LastUpdated = DateTime.Now,                          
+                      });
+                    await databaseContext.SaveChangesAsync();
+                }
+            }
+            return databaseContext;
+        }
+
+        [Fact]
+        public async void OfferRepository_GetByRequstObject_ReturnsOffer()
+        {
+            //Arrange
+            var dbContext = await GetDbContext();
+            var offerRepository = new OfferRepository(dbContext);
+
+            RequestObject requestObject = new RequestObject()
+            {
+                DateForm = DateTime.Now,
+                Duration = 10,
+                PeopleCount = "10",
+            };
+
+            //Act
+            var result = offerRepository.GetByRequstObject(requestObject);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<Task<IEnumerable<Offer>>>();
+        }
+    }
+}
---
+++ Test-IOMundo/Repository/OfferRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test_IOMundo.Data;
+using Test_IOMundo.Interfaces;
+using Test_IOMundo.Models;
+using Test_IOMundo.ViewModels;
+
+namespace Test_IOMundo.Repository
+{
+    public class OfferRepository : IOfferRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OfferRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Offer>> GetByRequstObject(RequestObject requestObject)
+        {
+            return await _context.Offers.Where(of => of.CheckInDate == requestObject.DateForm &&
+            of.StayDurationNights == requestObject.Duration &&
+            of.PersonCombination == requestObject.PeopleCount)
+            .ToListAsync();
+        }
+    }
+}
