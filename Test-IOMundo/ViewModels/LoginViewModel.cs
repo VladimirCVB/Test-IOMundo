95f549de4b0b20c3dfd7e624f043668b8e4705be
VladimirCVB <abuhnoaeivladimir@gmail.com>
2023-07-31 14:11:04 +0300

    feat: Add Login View Model
    Add login view model to be used for account login validation.

---
+++ Test-IOMundo/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Test_IOMundo.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "User name")]
+        [Required(ErrorMessage = "User name is required")]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
