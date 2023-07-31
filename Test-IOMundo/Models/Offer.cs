d0e3e81a61dc57f02bd2eb331628ebb16c2b6145
VladimirCVB <abuhnoaeivladimir@gmail.com>
2023-07-31 11:26:31 +0300

    feat: Add Offer model
    Add Offer model.

---
+++ Test-IOMundo/Models/Offer.cs
@@ -0,0 +1,21 @@
+﻿namespace Test_IOMundo.Models
+{
+    public class Offer
+    {
+        public string Id { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public int StayDurationNights { get; set; }
+        public string PersonCombination { get; set; }
+        public string ServiceCode { get; set; }
+        public int Price { get; set; }
+        public int PricePerAdult { get; set; }
+        public int PricePerChild { get; set; }
+        public int StrikePrice { get; set; }
+        public int StrikePricePerAdult { get; set; }
+
+        public int StrikePricePerChild { get; set; }
+        public bool ShowStrikePrice { get; set; }
+        public DateTime LastUpdated { get; set; }
+
+    }
+}
