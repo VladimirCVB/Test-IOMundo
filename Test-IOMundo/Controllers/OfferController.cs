25e388151538ea2d0df7fed12a96416afeeb8119
VladimirCVB <abuhnoaeivladimir@gmail.com>
2023-07-31 14:14:42 +0300

    feat: Add login verification to OfferController
    Users are verified before a list of offers is retrieved.

--- Test-IOMundo/Controllers/OfferController.cs
+++ Test-IOMundo/Controllers/OfferController.cs
@@ -8,13 +8,16 @@ namespace Test_IOMundo.Controllers
     public class OfferController : Controller
     {
         private readonly IOfferRepository _offerRepository;
+        private readonly AccountController _accountController;
 
-        public OfferController(IOfferRepository offerRepository)
+        public OfferController(IOfferRepository offerRepository, AccountController accountController)
         {
             _offerRepository = offerRepository;
+            _accountController = accountController;
         }
-        public async Task<IActionResult> SearchAvailability(RequestObject requestObject)
+        public async Task<IActionResult?> SearchAvailability(RequestObject requestObject)
         {
+            if (await _accountController.Login(requestObject.LoginViewModel) == false) return null;
             IEnumerable<Offer> offers = await _offerRepository.GetByRequstObject(requestObject);
             return View(offers);
         }
