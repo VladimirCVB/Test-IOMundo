d4534f4cf5247158698dc46133ae67b6037a1b7b
VladimirCVB <abuhnoaeivladimir@gmail.com>
2023-07-31 13:37:55 +0300

    feat: Add RequestObject to ViewModels
    Add RequestObject as view model to be used in order to filter Offers.

---
+++ Test-IOMundo/ViewModels/RequestObject.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Test_IOMundo.ViewModels
+{
+    public class RequestObject
+    {
+        [Display(Name = "Check-In Date")]
+        [Required(ErrorMessage = "Date is required")]
+        public DateTime DateForm { get; set; }
+
+        [Display(Name = "Duration")]
+        [Required(ErrorMessage = "Duration is required")]
+        public int Duration { get; set; }
+
+        [Display(Name = "Person Count")]
+        [Required(ErrorMessage = "Person Count is required")]
+        public string PeopleCount { get; set; }
+    }
+}
