7a357eea52ccf2b80497d3cb7b6b634b1a697886
VladimirCVB <abuhnoaeivladimir@gmail.com>
2023-07-31 16:42:50 +0300

    feat: Add price model
    Add price model to be used for the data seed.

---
+++ Test-IOMundo/Models/Price.cs
@@ -0,0 +1,22 @@
+﻿namespace Test_IOMundo.Models
+{
+    public class Price
+    {
+        public Price() { }
+        public Price(int adultDayPrice, int childDayPrice)
+        {
+            OneDayPricePerAdult = adultDayPrice;
+            OneDayPricePerChild = childDayPrice;
+        }
+        public int TotalPrice { get; set; } = 0;
+        public int PricePerAdult { get; set; } = 0;
+        public int PricePerChild { get; set; } = 0;
+        public int OneDayPricePerAdult { get; } = 100;
+        public int OneDayPricePerChild { get; } = 50;
+
+        public int calculateTotalPrice()
+        {
+            return PricePerAdult + PricePerChild;
+        }
+    }
+}
