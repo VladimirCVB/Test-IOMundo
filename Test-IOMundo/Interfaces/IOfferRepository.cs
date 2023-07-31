69a47b71764b4c445a10a693e7fd4e9a7232734b
VladimirCVB <abuhnoaeivladimir@gmail.com>
2023-07-31 13:48:46 +0300

    feat: Add Offer repository interface
    Setup offer repository interface.

---
+++ Test-IOMundo/Interfaces/IOfferRepository.cs
@@ -0,0 +1,10 @@
+﻿using Test_IOMundo.Models;
+using Test_IOMundo.ViewModels;
+
+namespace Test_IOMundo.Interfaces
+{
+    public interface IOfferRepository
+    {
+        Task<IEnumerable<Offer>> GetByRequstObject(RequestObject requestObject);
+    }
+}
