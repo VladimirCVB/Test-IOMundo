1bf49ec9269ed36cfc9e29f7903d55591267a516
VladimirCVB <abuhnoaeivladimir@gmail.com>
2023-07-31 14:55:32 +0300

    feat: Add offer and account services to build
    Add offer and account services to build.

--- Test-IOMundo/Program.cs
+++ Test-IOMundo/Program.cs
@@ -1,10 +1,24 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Test_IOMundo.Data;
+using Test_IOMundo.Interfaces;
+using Test_IOMundo.Models;
+using Test_IOMundo.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IOfferRepository, OfferRepository>();
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie();
 
 builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseNpgsql(builder.Configuration.GetConnectionString("IOMundo"))
