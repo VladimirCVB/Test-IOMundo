b19cb864d8e728af16e0fc7cc3efd941a584f2f6
VladimirCVB <abuhnoaeivladimir@gmail.com>
2023-07-31 14:10:11 +0300

    feat: Add Account Controller to provide login for user
    Added Account Controller to enable login for users.

---
+++ Test-IOMundo/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Test_IOMundo.Data;
+using Test_IOMundo.Models;
+using Test_IOMundo.ViewModels;
+
+namespace Test_IOMundo.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly ApplicationDbContext _context;
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, ApplicationDbContext context)
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Login()
+        {
+            var response = new LoginViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<bool> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid) return false;
+
+            var user = await _userManager.FindByNameAsync(loginViewModel.UserName);
+
+            if (user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginViewModel.Password);
+                if (passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            return false;
+        }
+    }
+}
