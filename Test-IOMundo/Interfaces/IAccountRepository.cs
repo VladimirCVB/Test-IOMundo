f7ce6c2c6b23045192609d95504e7fc150a82f9b
VladimirCVB <abuhnoaeivladimir@gmail.com>
2023-07-31 14:52:47 +0300

    feat: Add Acount Repository Interface
    Add Acount Repository Interface.

---
+++ Test-IOMundo/Interfaces/IAccountRepository.cs
@@ -0,0 +1,9 @@
+﻿using Test_IOMundo.ViewModels;
+
+namespace Test_IOMundo.Interfaces
+{
+    public interface IAccountRepository
+    {
+        Task<bool> Login(LoginViewModel loginViewModel);
+    }
+}
